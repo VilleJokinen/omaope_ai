217da6a96404a52f53ca3d35bdb58608f31d7ed8
Ville Jokinen <villeejjokinen@gmail.com>
2024-09-23 19:29:25 +0300

    the msgs now go in the chatbox

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,23 @@
 document.getElementById('chatbot-send-button').addEventListener('click', sendChatMessage);
 
+document.getElementById('chatbot-user-input').addEventListener('keypress', function (keyPressed) {
+    if (keyPressed.key === 'Enter') {
+        sendChatMessage();
+    }
+});
+
 function sendChatMessage() {
     console.log("sendChatMessage");
+    var userChatMessage = document.getElementById('chatbot-user-input').value;
+    console.log(userChatMessage);
+    document.getElementById('chatbot-user-input').value = '';
+    addMessageToChatbox(userChatMessage);
+}
+
+function addMessageToChatbox(message){
+    console.log("viesti lisätty chatboxii ")
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    console.log(messageElement);
+    document.getElementById('chatbox').appendChild(messageElement);
 }
